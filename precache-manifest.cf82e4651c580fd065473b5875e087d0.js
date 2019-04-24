3e950e6cc1e251f06948f3f1876dd61e5a38b709
harshika <hgupta@cainc.com>
2019-04-24 23:03:01 +0530

    Updates

--- precache-manifest.cf82e4651c580fd065473b5875e087d0.js
+++ precache-manifest.cf82e4651c580fd065473b5875e087d0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/Avatar-app/static/js/runtime~main.153c69fa.js"
   },
   {
-    "revision": "dc38053e847fe2183a53",
-    "url": "/Avatar-app/static/js/main.31b4a6d9.chunk.js"
+    "revision": "0fc394e78699b84a42f1",
+    "url": "/Avatar-app/static/js/main.beff02ff.chunk.js"
   },
   {
     "revision": "1dd0d98a2a7a28b7ba63",
     "url": "/Avatar-app/static/js/2.742d9893.chunk.js"
   },
   {
-    "revision": "dc38053e847fe2183a53",
+    "revision": "0fc394e78699b84a42f1",
     "url": "/Avatar-app/static/css/main.aaf7d7da.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Avatar-app/static/css/2.86e5aaf1.chunk.css"
   },
   {
-    "revision": "353b1ad9b5d8c19ec8048f1dc71c91cf",
+    "revision": "cd1c694a53c946aa3deafd64c8eeba14",
     "url": "/Avatar-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Avatar-app/precache-manifest.ebe6921681d0714c4f8acdff4dc75930.js"
+  "/Avatar-app/precache-manifest.cf82e4651c580fd065473b5875e087d0.js"
 );
 
 workbox.clientsClaim();
